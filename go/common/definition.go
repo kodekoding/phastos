9837e2eebbab855344111652855a96e86d41f832
radityaapratamaa <radityaapratamaa@gmail.com>
2022-01-22 01:56:44 +0700

    feat(definitions): common interface for each layer

---
+++ go/common/definition.go
@@ -0,0 +1,50 @@
+package common
+
+import (
+	"context"
+	"database/sql"
+	"net/http"
+
+	"github.com/kodekoding/phastos/database"
+	"github.com/kodekoding/phastos/response"
+)
+
+type ReadRepo interface {
+	GetList(ctx context.Context, requestData interface{}) (interface{}, error)
+	GetDetailById(ctx context.Context, id int) (interface{}, error)
+}
+
+type WriteRepo interface {
+	Insert(ctx context.Context, data *database.CUDConstructData, trx ...*sql.Tx) (*database.CUDResponse, error)
+	Update(ctx context.Context, data *database.CUDConstructData, trx ...*sql.Tx) (*database.CUDResponse, error)
+	Delete(ctx context.Context, id int, trx ...*sql.Tx) (*database.CUDResponse, error)
+}
+
+type ReadHandler interface {
+	GetList(w http.ResponseWriter, r *http.Request) *response.JSON
+	GetDetailById(w http.ResponseWriter, r *http.Request) *response.JSON
+}
+
+type WriteHandler interface {
+	Insert(w http.ResponseWriter, r *http.Request) *response.JSON
+	Update(w http.ResponseWriter, r *http.Request) *response.JSON
+	Delete(w http.ResponseWriter, r *http.Request) *response.JSON
+}
+
+type HandlerCRUD interface {
+	ReadHandler
+	WriteHandler
+}
+
+type RepoCRUD interface {
+	ReadRepo
+	WriteRepo
+}
+
+type UsecaseCRUD interface {
+	GetList(ctx context.Context, requestData interface{}) (*database.SelectResponse, error)
+	GetDetailById(ctx context.Context, id int) (*database.SelectResponse, error)
+	Insert(ctx context.Context, data interface{}) (*database.CUDResponse, error)
+	Update(ctx context.Context, data interface{}) (*database.CUDResponse, error)
+	Delete(ctx context.Context, id int) (*database.CUDResponse, error)
+}
