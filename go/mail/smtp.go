ef620defd978c88c3b4cf25c8059aeb70e50401f
radityaapratamaa <radityaapratamaa@gmail.com>
2022-03-16 13:14:02 +0700

    feat(mail): adding SMTP mail library

---
+++ go/mail/smtp.go
@@ -0,0 +1,83 @@
+package mail
+
+import (
+	"fmt"
+	"net/smtp"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type (
+	SMTPs interface {
+		SetRecipient(recipient []string) SMTPs
+		SetContent(subject, message string) SMTPs
+		Send() error
+	}
+
+	SMTP struct {
+		auth smtp.Auth
+		SMTPConfig
+		err error
+	}
+
+	SMTPConfig struct {
+		Sender        string
+		EmailUsername string
+		EmailPassword string
+		EmailFrom     string
+		Host          string
+		Port          int
+		subject       string
+		recipient     []string
+		message       string
+		address       string
+	}
+)
+
+func NewSMTP(cfg *SMTPConfig) SMTPs {
+	auth := smtp.PlainAuth("", cfg.EmailUsername, cfg.EmailPassword, cfg.Host)
+	cfg.address = fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	return &SMTP{
+		SMTPConfig: *cfg,
+		auth:       auth,
+	}
+}
+
+func (s *SMTP) SetRecipient(recipient []string) SMTPs {
+	if recipient == nil {
+		s.err = errors.New("recipient params cannot be null")
+		return s
+	}
+	s.recipient = recipient
+	return s
+}
+
+func (s *SMTP) SetContent(subject, message string) SMTPs {
+	if s.Sender == "" || s.recipient == nil {
+		s.err = errors.New("sender name or recipient must be filled")
+		return s
+	}
+	s.message = fmt.Sprintf(`
+		MIME-version: 1.0;
+		Content-Type: text/html; charset="UTF-8";
+		From: %s
+		To: %s
+		Subject: %s
+
+		%s
+	`, s.Sender, strings.Join(s.recipient, ","), subject, message)
+	return s
+}
+
+func (s *SMTP) Send() error {
+	if s.err != nil {
+		return s.err
+	}
+
+	if err := smtp.SendMail(s.address, s.auth, s.EmailFrom, s.recipient, []byte(s.message)); err != nil {
+		return err
+	}
+
+	return nil
+}
