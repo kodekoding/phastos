4db2de570db795d938f4300696c321beb3a6c353
radityaapratamaa <radityaapratamaa@gmail.com>
2022-04-02 18:52:59 +0700

    feat(server): adding graceful handling for server shutdown

---
+++ go/server/definition.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"context"
+	"net"
+	"net/http"
+)
+
+type (
+	// HTTP represents interface for HTTP/s server
+	HTTP interface {
+		Shutdown(ctx context.Context) error
+		ListenAndServer() error
+		ListenAndServerTLS(certFile, keyFile string) error
+	}
+
+	// GRPC represents interface for grpc server
+	GRPC interface {
+		GracefulStop()
+		Stop()
+		Serve(l net.Listener) error
+	}
+
+	Config struct {
+		Port          int `yaml:"port"`
+		ReadTimeout   int `yaml:"read_timeout"`
+		WriteTimeout  int `yaml:"write_timeout"`
+		MaxHeaderByte int `yaml:"max_header_byte"`
+		Handler       http.Handler
+		CertFile      string
+		KeyFile       string
+	}
+)
---
+++ go/server/graceful.go
@@ -0,0 +1,96 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func ServeHTTP(config *Config) error {
+	return serveHTTPs(config, false)
+}
+
+func ServeHTTPS(config *Config) error {
+	return serveHTTPs(config, true)
+}
+
+func serveHTTPs(config *Config, secure bool) error {
+	listenPort := fmt.Sprintf(":%d", config.Port)
+	server := &http.Server{
+		Addr:           listenPort,
+		ReadTimeout:    time.Duration(config.ReadTimeout) * time.Second,
+		WriteTimeout:   time.Duration(config.WriteTimeout) * time.Second,
+		Handler:        config.Handler,
+		MaxHeaderBytes: config.MaxHeaderByte,
+	}
+
+	shutdownSignal := WaitTermSig(func(ctx context.Context) error {
+		stopped := make(chan struct{})
+		ctx, cancel := context.WithTimeout(ctx, time.Duration(10)*time.Second)
+		defer cancel()
+		go func() {
+			_ = server.Shutdown(ctx)
+			close(stopped)
+		}()
+
+		select {
+		case <-ctx.Done():
+			return errors.New("server shutdown timed out")
+		case <-stopped:
+
+		}
+
+		return nil
+	})
+
+	go func() {
+		if secure {
+			if err := server.ListenAndServeTLS(config.CertFile, config.KeyFile); err != nil {
+				log.Fatalf("Cannot Listen and Serve: %s", err.Error())
+			}
+		} else {
+			if err := server.ListenAndServe(); err != nil {
+				log.Fatalf("Cannot Listen and Serve: %s", err.Error())
+			}
+		}
+	}()
+
+	protocol := "HTTP"
+	if secure {
+		protocol += "s"
+	}
+	log.Printf("%s Server is running on %s", protocol, listenPort)
+
+	<-shutdownSignal
+
+	log.Printf("%s Server stopped", protocol)
+	return nil
+}
+
+func WaitTermSig(handler func(context.Context) error) <-chan struct{} {
+	stoppedCh := make(chan struct{})
+	go func() {
+		signals := make(chan os.Signal, 1)
+
+		// wait for the sigterm
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+		<-signals
+
+		// We received an os signal, shut down.
+		if err := handler(context.Background()); err != nil {
+			log.Printf("graceful shutdown  failed: %v", err)
+		} else {
+			log.Println("gracefull shutdown succeed")
+		}
+
+		close(stoppedCh)
+
+	}()
+	return stoppedCh
+}
