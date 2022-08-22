a4b4a805d9932811d722fff9d0fc6ede998804aa
radityaapratamaa <radityaapratamaa@gmail.com>
2022-08-23 02:37:25 +0700

    feat(common-constants): adding `errPatch`

--- go/common/constants.go
+++ go/common/constants.go
@@ -6,4 +6,5 @@ var (
 	ErrStructNotCompatible = errors.New("struct is not compatible")
 	ErrIdParamIsRequired   = errors.New("parameter ID is required")
 	ErrIdMustNumeric       = errors.New("ID must be defined as number")
+	ErrPatch               = errors.New("error patch")
 )
