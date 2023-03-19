2d429585bafe94009142d2ab0b1646e4078597b5
Raditya Pratama <radityaapratamaa@gmail.com>
2023-03-19 19:17:24 +0700

    feat(slack): adding new some function on slack app library

--- go/apps/slack.go
+++ go/apps/slack.go
@@ -1,17 +1,16 @@
 package apps
 
 import (
+	"context"
 	"github.com/pkg/errors"
-	"log"
-	"os"
-
 	slackpkg "github.com/slack-go/slack"
 )
 
 type Slacks interface {
-	CreateNewChannel(name string, isPrivate ...bool) (*slackpkg.Channel, error)
-	InviteUserToChannel(channelId string, users ...string) error
-	ArchiveChannel(channelId string) error
+	CreateNewChannel(ctx context.Context, name string, isPrivate ...bool) (*slackpkg.Channel, error)
+	InviteUserToChannel(ctx context.Context, channelId string, users ...string) error
+	ArchiveChannel(ctx context.Context, channelId string) error
+	AddReminderToChannel(ctx context.Context, channelId, textReminder, time string) error
 }
 
 type slack struct {
@@ -19,37 +18,40 @@ type slack struct {
 }
 
 func NewSlack(botToken string) *slack {
-	client := slackpkg.New(
-		botToken,
-		slackpkg.OptionDebug(true),
-		slackpkg.OptionLog(log.New(os.Stdout, "sertif-api: ", log.Lshortfile|log.LstdFlags)),
-	)
-
+	client := slackpkg.New(botToken, slackpkg.OptionDebug(true))
 	return &slack{client: client}
 }
 
 // CreateNewChannel - create new channel slack, with params:
 // - name: channel name
 // - isPrivate: is private channel ?? default is false, then should be create public channel
-func (s *slack) CreateNewChannel(name string, isPrivate ...bool) (*slackpkg.Channel, error) {
+func (s *slack) CreateNewChannel(ctx context.Context, name string, isPrivate ...bool) (*slackpkg.Channel, error) {
 	isPrivateChannel := false
 	if isPrivate != nil && len(isPrivate) > 0 {
 		isPrivateChannel = isPrivate[0]
 	}
-	return s.client.CreateConversation(slackpkg.CreateConversationParams{
+	return s.client.CreateConversationContext(ctx, slackpkg.CreateConversationParams{
 		ChannelName: name,
 		IsPrivate:   isPrivateChannel,
 	})
 }
 
-func (s *slack) InviteUserToChannel(channelId string, users ...string) error {
-	if _, err := s.client.InviteUsersToConversation(channelId, users...); err != nil {
+func (s *slack) InviteUserToChannel(ctx context.Context, channelId string, users ...string) error {
+	if _, err := s.client.InviteUsersToConversationContext(ctx, channelId, users...); err != nil {
 		return errors.Wrap(err, "phastos.go.apps.slack.InviteUserToChannel")
 	}
 	return nil
 }
 
 // ArchiveChannel - archive channel slack
-func (s *slack) ArchiveChannel(channelId string) error {
-	return s.client.ArchiveConversation(channelId)
+func (s *slack) ArchiveChannel(ctx context.Context, channelId string) error {
+	return s.client.ArchiveConversationContext(ctx, channelId)
+}
+
+// AddReminderToChannel - adding reminder to channel slack by ID
+func (s *slack) AddReminderToChannel(ctx context.Context, channelId, textReminder, time string) error {
+	if _, err := s.client.AddChannelReminderContext(ctx, channelId, textReminder, time); err != nil {
+		return errors.Wrap(err, "phastos.go.apps.slack.AddReminderToChannel")
+	}
+	return nil
 }
