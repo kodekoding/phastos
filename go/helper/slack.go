35028c7d0846a815169cd9815ae8c73af72796be
Raditya Pratama <radityaapratamaa@gmail.com>
2023-03-30 00:54:25 +0700

    feat(helper): separate parseTemplate to new function

--- go/helper/slack.go
+++ go/helper/slack.go
@@ -1,40 +1,16 @@
 package helper
 
 import (
-	"bytes"
 	"embed"
 	"encoding/json"
 	"github.com/pkg/errors"
-	"html/template"
-	"io"
 )
 
 func GetTemplate(embedFS embed.FS, file string, args, destStruct interface{}) error {
 
-	var templateValue []byte
-	var err error
-	if args != nil {
-
-		// read the block-kit definition as a go template
-		var tpl bytes.Buffer
-		t, err := template.ParseFS(embedFS, file)
-		if err != nil {
-			return errors.Wrap(err, "phastos.go.third_party.slack.templating.GetTemplate.ParseFS")
-		}
-
-		// we render the view
-		err = t.Execute(&tpl, args)
-		if err != nil {
-			return errors.Wrap(err, "phastos.go.third_party.slack.templating.GetTemplate.ExecuteTemplate")
-		}
-		templateValue, err = io.ReadAll(&tpl)
-		if err != nil {
-			return errors.Wrap(err, "phastos.go.third_party.slack.templating.GetTemplate.ReadAllTemplate")
-		}
-	} else {
-		if templateValue, err = embedFS.ReadFile(file); err != nil {
-			return errors.Wrap(err, "phastos.go.third_party.slack.templating.GetTemplate.ReadFile")
-		}
+	templateValue, err := ParseTemplate(embedFS, file, args)
+	if err != nil {
+		return errors.Wrap(err, "phastos.go.helper.slack.GetTemplate.ParseTemplate")
 	}
 
 	if err = json.Unmarshal(templateValue, destStruct); err != nil {
