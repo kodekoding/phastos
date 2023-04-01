24d902662e267f8cf55930439beeb037872ead56
Raditya Pratama <radityaapratamaa@gmail.com>
2023-04-02 02:43:23 +0700

    refactor(generator): refactored generator banner logic

--- go/generator/banner.go
+++ go/generator/banner.go
@@ -1,32 +1,37 @@
 package generator
 
 import (
-	"github.com/golang/freetype"
-	"github.com/kodekoding/phastos/go/helper"
-	"github.com/kodekoding/phastos/go/log"
-	"github.com/pkg/errors"
 	"image"
 	"image/color"
 	"image/draw"
+	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"os"
 	"strings"
+
+	"github.com/fogleman/gg"
+	"github.com/pkg/errors"
+
+	"github.com/kodekoding/phastos/go/helper"
+	"github.com/kodekoding/phastos/go/log"
 )
 
 type (
 	Banners interface {
-		SetBgColor(colorParam color.Color) Banners
-		SetBgHexColor(hexColor string) Banners
 		AddImageLayer(img *ImageLayer) Banners
 		AddLabel(labelText *Label) Banners
-		Generate(savePath string) error
+		Generate() Banners
+		Image() image.Image
+		Save(destPath string) error
 	}
 
-	Banner struct {
+	Options func(*Banner)
+	Banner  struct {
 		imgLayer []*ImageLayer
 		label    []*Label
 		BgProperty
+		img image.Image
 	}
 	//ImageLayer is a struct
 	ImageLayer struct {
@@ -37,46 +42,62 @@ type (
 
 	//Label is a struct
 	Label struct {
-		Text     string
-		FontPath string
-		FontType string
-		Size     float64
-		Color    image.Image
-		DPI      float64
-		Spacing  float64
-		XPos     int
-		YPos     int
+		Text        string
+		FontPath    string
+		FontType    string
+		Size        float64
+		Color       color.Color
+		DPI         float64
+		Spacing     float64
+		XPos        int
+		YPos        int
+		RightMargin float64
 	}
 
 	//BgProperty is background property struct
 	BgProperty struct {
 		Width   int
-		Length  int
+		Height  int
 		BgColor color.Color
 	}
 )
 
-func NewBanner(width int, length int) *Banner {
-	return &Banner{
+// NewBanner - by default will be w: 1200, h: 400, bgColor: white
+func NewBanner(opts ...Options) *Banner {
+	defaultBgColor, _ := helper.ParseHexColor("#fff")
+	bannerObj := &Banner{
 		BgProperty: BgProperty{
-			Width:  width,
-			Length: length,
+			Width:   1200,
+			Height:  400,
+			BgColor: defaultBgColor,
 		},
 	}
-}
-
-func (b *Banner) SetBgColor(colorParam color.Color) Banners {
-	b.BgColor = colorParam
-	return b
-}
-
-func (b *Banner) SetBgHexColor(hexColor string) Banners {
-	rgba, err := helper.ParseHexColor(hexColor)
-	if err != nil {
-		log.Errorln("got error when parse hex string: ", err.Error())
+	for _, opt := range opts {
+		opt(bannerObj)
 	}
-	b.BgColor = rgba
-	return b
+	return bannerObj
+}
+
+func WithWidth(width int) Options {
+	return func(banner *Banner) {
+		banner.Width = width
+	}
+}
+
+func WithHeight(height int) Options {
+	return func(banner *Banner) {
+		banner.Height = height
+	}
+}
+
+func WithBackgroudColor(hexColor string) Options {
+	return func(banner *Banner) {
+		rgba, err := helper.ParseHexColor(hexColor)
+		if err != nil {
+			log.Errorln("got error when parse hex string: ", err.Error())
+		}
+		banner.BgColor = rgba
+	}
 }
 
 func (b *Banner) AddImageLayer(img *ImageLayer) Banners {
@@ -89,89 +110,71 @@ func (b *Banner) AddLabel(labelText *Label) Banners {
 	return b
 }
 
-func (b *Banner) Generate(savePath string) error {
+func (b *Banner) Image() image.Image {
+	return b.img
+}
+
+func (b *Banner) Generate() Banners {
 	//create image's background
-	bgImg := image.NewRGBA(image.Rect(0, 0, b.Width, b.Length))
+	dc := gg.NewContext(b.Width, b.Height)
+	bgImg := image.NewRGBA(image.Rect(0, 0, b.Width, b.Height))
 
 	//set the background color
 	draw.Draw(bgImg, bgImg.Bounds(), &image.Uniform{b.BgColor}, image.Point{}, draw.Src)
-
+	dc.DrawImage(bgImg, 0, 0)
 	//looping image layer, higher array index = upper layer
 	for _, img := range b.imgLayer {
-		//set image offset
-		offset := image.Pt(img.XPos, img.YPos)
-
-		//combine the image
-		draw.Draw(bgImg, img.Image.Bounds().Add(offset), img.Image, image.Point{}, draw.Over)
+		dc.DrawImage(img.Image, img.XPos, img.YPos)
 	}
 
-	//add label(s)
-	bgImg, err := b.addLabel(bgImg, b.label)
-	if err != nil {
-		return err
+	for _, label := range b.label {
+		if err := dc.LoadFontFace(label.FontPath, label.Size); err != nil {
+			log.Errorln("got error when load font face: ", err.Error())
+			return nil
+		}
+
+		x := float64(label.XPos)
+		y := float64(label.YPos)
+		maxWidth := float64(dc.Width()) - label.RightMargin - label.RightMargin
+		dc.DrawStringWrapped(label.Text, x+1, y+1, 0, 0, maxWidth, label.Spacing, gg.AlignLeft)
+		dc.SetColor(label.Color)
+		dc.DrawStringWrapped(label.Text, x, y, 0, 0, maxWidth, label.Spacing, gg.AlignLeft)
 	}
 
-	newFile, err := os.Create(savePath)
+	b.img = dc.Image()
+	return b
+}
+
+func (b *Banner) Save(destPath string) error {
+	newFile, err := os.Create(destPath)
 	if err != nil {
 		return errors.Wrap(err, "phastos.go.generator.banner.Generate.CreateNewFile")
 	}
 	defer newFile.Close()
 
-	splitSavePath := strings.Split(savePath, ".")
+	splitSavePath := strings.Split(destPath, ".")
 	saveFileExt := splitSavePath[len(splitSavePath)-1]
 	switch saveFileExt {
 	case "png":
-		if err = png.Encode(newFile, bgImg); err != nil {
+		if err = png.Encode(newFile, b.img); err != nil {
 			return errors.Wrap(err, "phastos.go.generator.banner.Generate.EncodeToPNG")
 		}
 	case "jpg", "jpeg":
 		var opt jpeg.Options
 		opt.Quality = 80
 
-		if err = jpeg.Encode(newFile, bgImg, &opt); err != nil {
+		if err = jpeg.Encode(newFile, b.img, &opt); err != nil {
 			return errors.Wrap(err, "phastos.go.generator.banner.Generate.EncodeToJPEG")
+		}
+	case "gif":
+		var opt gif.Options
+
+		if err = gif.Encode(newFile, b.img, &opt); err != nil {
+			return errors.Wrap(err, "phastos.go.generator.banner.Generate.EncodeToGif")
 		}
 	default:
 		return errors.Wrap(errors.New("file extensions isn't support yet"), "phastos.go.generator.banner.Generate.NewFileExtensionCheck")
 	}
 
 	return nil
-}
-
-func (b *Banner) addLabel(img *image.RGBA, labels []*Label) (*image.RGBA, error) {
-	//initialize the context
-	c := freetype.NewContext()
-
-	for _, label := range labels {
-		//read font data
-		fontBytes, err := os.ReadFile(label.FontPath + label.FontType)
-		if err != nil {
-			return nil, err
-		}
-		f, err := freetype.ParseFont(fontBytes)
-		if err != nil {
-			return nil, err
-		}
-
-		//set label configuration
-		c.SetDPI(label.DPI)
-		c.SetFont(f)
-		c.SetFontSize(label.Size)
-		c.SetClip(img.Bounds())
-		c.SetDst(img)
-		c.SetSrc(label.Color)
-
-		//positioning the label
-		pt := freetype.Pt(label.XPos, label.YPos+int(c.PointToFixed(label.Size)>>6))
-
-		//draw the label on image
-		_, err = c.DrawString(label.Text, pt)
-		if err != nil {
-			log.Errorln("got error when draw string", err)
-			return img, nil
-		}
-		pt.Y += c.PointToFixed(label.Size * label.Spacing)
-	}
-
-	return img, nil
 }
