6b99c6ce94462e368d330c2e82214ea1c23f9d57
Raditya Pratama <radityaapratamaa@gmail.com>
2023-07-03 00:22:22 +0700

    refactor(helper): adding logic when gcs is fulfill then upload tmp file to cloud storage

--- go/helper/upload.go
+++ go/helper/upload.go
@@ -7,28 +7,49 @@ import (
 	"mime/multipart"
 	"os"
 
-	"github.com/kodekoding/phastos/go/env"
-
 	"github.com/pkg/errors"
+
+	"github.com/kodekoding/phastos/go/env"
+	"github.com/kodekoding/phastos/go/storage"
 )
 
-func UploadToTmp(_ context.Context, path *string, file multipart.File) error {
+type CloudStorageTmpUpload struct {
+	Storage       storage.Buckets
+	TmpBucketName string
+}
+
+func UploadToTmp(ctx context.Context, path *string, file multipart.File, cloudStorage ...*CloudStorageTmpUpload) error {
 	basePath := ""
 	if env.IsLocal() {
 		basePath = "files"
 	}
+	basePath += "/"
 
-	*path = fmt.Sprintf("%s/tmp/%s", basePath, *path)
-	CheckFolder(*path)
-
-	tmpFile, err := os.Create(*path)
-	if err != nil {
-		return errors.Wrap(err, "pkg.helper.upload.UploadToTmp.CreateFile")
+	isTmpToCloudStorage := cloudStorage != nil && len(cloudStorage) > 0
+	if isTmpToCloudStorage {
+		basePath = ""
 	}
-	defer tmpFile.Close()
 
-	if _, err = io.Copy(tmpFile, file); err != nil {
-		return errors.Wrap(err, "pkg.helper.upload.UploadToTmp.CopyToDestinationFile")
+	if isTmpToCloudStorage {
+		*path = fmt.Sprintf("tmp/%s", *path)
+		cloudStorageOption := cloudStorage[0]
+		store := cloudStorageOption.Storage
+		if err := store.SetBucketName(cloudStorageOption.TmpBucketName).UploadFile(ctx, file, path); err != nil {
+			return errors.Wrap(err, "phastos.go.helper.upload.UploadToTmp.UploadFileGCS")
+		}
+	} else {
+		*path = fmt.Sprintf("%stmp/%s", basePath, *path)
+		CheckFolder(*path)
+
+		tmpFile, err := os.Create(*path)
+		if err != nil {
+			return errors.Wrap(err, "pkg.helper.upload.UploadToTmp.CreateFile")
+		}
+		defer tmpFile.Close()
+
+		if _, err = io.Copy(tmpFile, file); err != nil {
+			return errors.Wrap(err, "phastos.go.helper.upload.UploadToTmp.CopyToDestinationFile")
+		}
 	}
 
 	return nil
