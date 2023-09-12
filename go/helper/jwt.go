4235adaf8e106302097b5b6490631fbc1f727912
Raditya Pratama <radityaapratamaa@gmail.com>
2023-09-12 22:21:38 +0700

    chore(generate-jwt-token): adding expireTime user-defined

--- go/helper/jwt.go
+++ go/helper/jwt.go
@@ -2,15 +2,19 @@ package helper
 
 import (
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/kodekoding/phastos/v2/go/common"
 	"github.com/kodekoding/phastos/v2/go/entity"
 	"github.com/pkg/errors"
 	"os"
 	"time"
 )
 
-func GenerateJWTToken(data interface{}) (string, error) {
+func GenerateJWTToken(data interface{}, expireTime ...time.Duration) (string, error) {
 	nowTime := time.Now()
-	expireTime := nowTime.Add(24 * time.Hour)
+	expiredTime := nowTime.Add(24 * time.Hour)
+	if expireTime != nil && len(expireTime) > 0 {
+		expiredTime = nowTime.Add(expireTime[0])
+	}
 
 	defaultIssuer := "phastos"
 	jwtIssuer := os.Getenv("JWT_ISSUER")
@@ -20,12 +24,12 @@ func GenerateJWTToken(data interface{}) (string, error) {
 	claimData := new(entity.JWTClaimData)
 	claimData.Data = data
 	claimData.RegisteredClaims = jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(expireTime),
+		ExpiresAt: jwt.NewNumericDate(expiredTime),
 		Issuer:    jwtIssuer,
 	}
 
 	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claimData)
-	token, err := tokenClaims.SignedString([]byte(os.Getenv("JWT_SIGNING_KEY")))
+	token, err := tokenClaims.SignedString([]byte(os.Getenv(common.EnvJWTSigningKey)))
 	if err != nil {
 		return "", errors.Wrap(err, "lib.helper.jwt.GenerateJWTToken.SignedString")
 	}
