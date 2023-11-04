e04bb5912e2be0ac3b6c8a1ef468b7f8133d30fd
Raditya Pratama <radityaapratamaa@gmail.com>
2023-11-05 02:44:49 +0700

    refactor(cache-key): adding prefixKey for cache-key + make expire params on set function to optional

--- go/cache/redis.go
+++ go/cache/redis.go
@@ -2,7 +2,9 @@ package cache
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	redigo "github.com/gomodule/redigo/redis"
@@ -11,7 +13,8 @@ import (
 
 // Store object
 type Store struct {
-	Pool Handler
+	Pool      Handler
+	prefixKey string
 }
 
 type Options func(*RedisCfg)
@@ -30,6 +33,8 @@ type Handler interface {
 	Get() redigo.Conn
 	GetContext(context.Context) (redigo.Conn, error)
 }
+
+const defaultPrefixKey = "coduty:"
 
 type Caches interface {
 	Get(ctx context.Context, key string) (string, error)
@@ -81,6 +86,12 @@ func New(options ...Options) *Store {
 	if _, err := redigo.String(pool.Do("PING")); err != nil {
 		log.Fatalln("Cannot connect to redis: " + err.Error())
 	}
+
+	prefixKey := os.Getenv("REDIS_PREFIX_KEY")
+	if prefixKey == "" {
+		prefixKey = defaultPrefixKey
+	}
+	store.prefixKey = prefixKey
 	log.Println("Successful connect to redis")
 
 	return store
@@ -129,7 +140,7 @@ func (r *Store) Get(ctx context.Context, key string) (string, error) {
 		return "", errors.Wrap(err, "cache.redis.Get.GetContext")
 	}
 	defer conn.Close()
-	resp, err := redigo.String(conn.Do("GET", key))
+	resp, err := redigo.String(conn.Do("GET", fmt.Sprintf("%s%s", r.prefixKey, key)))
 	if err == redigo.ErrNil {
 		return "", errors.Wrap(err, "infrastructure.cache.redis.Get")
 	}
@@ -143,7 +154,7 @@ func (r *Store) Del(ctx context.Context, key string) (int64, error) {
 		return 0, errors.Wrap(err, "cache.redis.Del.GetContext")
 	}
 	defer conn.Close()
-	resp, err := redigo.Int64(conn.Do("DEL", key))
+	resp, err := redigo.Int64(conn.Do("DEL", fmt.Sprintf("%s%s", r.prefixKey, key)))
 	if err == redigo.ErrNil {
 		return 0, errors.Wrap(err, "infrastructure.cache.redis.Del")
 	}
@@ -157,17 +168,24 @@ func (r *Store) HSet(ctx context.Context, key, field, value string) (string, err
 		return "", errors.Wrap(err, "cache.redis.HSet.GetContext")
 	}
 	defer conn.Close()
-	return redigo.String(conn.Do("HSET", key, field, value))
+	return redigo.String(conn.Do("HSET", fmt.Sprintf("%s%s", r.prefixKey, key), field, value))
 }
 
 // Set ill be used to set the value
-func (r *Store) Set(ctx context.Context, key, value string, expire int) (string, error) {
+func (r *Store) Set(ctx context.Context, key, value string, expire ...int) (string, error) {
 	conn, err := r.Pool.GetContext(ctx)
 	if err != nil {
 		return "", errors.Wrap(err, "cache.redis.Set.GetContext")
 	}
 	defer conn.Close()
-	return redigo.String(conn.Do("SET", key, value, "EX", expire))
+	var setParams []interface{}
+	setParams = append(setParams, fmt.Sprintf("%s%s", r.prefixKey, key))
+	setParams = append(setParams, value)
+	if expire != nil && len(expire) > 0 {
+		setParams = append(setParams, "EX")
+		setParams = append(setParams, expire)
+	}
+	return redigo.String(conn.Do("SET", setParams...))
 }
 
 // AddInSet will be used to add value in set
@@ -177,7 +195,7 @@ func (r *Store) AddInSet(ctx context.Context, key, value string) (int, error) {
 		return 0, errors.Wrap(err, "cache.redis.AddInSet.GetContext")
 	}
 	defer conn.Close()
-	return redigo.Int(conn.Do("SADD", key, value))
+	return redigo.Int(conn.Do("SADD", fmt.Sprintf("%s%s", r.prefixKey, key), value))
 }
 
 // GetSetMembers will be used to get the set memebers
@@ -187,7 +205,7 @@ func (r *Store) GetSetMembers(ctx context.Context, key string) ([]string, error)
 		return nil, errors.Wrap(err, "cache.redis.GetSetMembers.GetContext")
 	}
 	defer conn.Close()
-	return redigo.Strings(conn.Do("SMEMBERS", key))
+	return redigo.Strings(conn.Do("SMEMBERS", fmt.Sprintf("%s%s", r.prefixKey, key)))
 }
 
 // GetSetLength will be used to get the set length
@@ -197,7 +215,7 @@ func (r *Store) GetSetLength(ctx context.Context, key string) (int, error) {
 		return 0, errors.Wrap(err, "cache.redis.GetSetLength.GetContext")
 	}
 	defer conn.Close()
-	return redigo.Int(conn.Do("SCARD", key))
+	return redigo.Int(conn.Do("SCARD", fmt.Sprintf("%s%s", r.prefixKey, key)))
 }
 
 // GetNElementOfSet to get the first N elements of set
@@ -207,7 +225,7 @@ func (r *Store) GetNElementOfSet(ctx context.Context, key string, n int) ([]stri
 		return nil, errors.Wrap(err, "cache.redis.GetNElementOfSet.GetContext")
 	}
 	defer conn.Close()
-	return redigo.Strings(conn.Do("SPOP", key, n))
+	return redigo.Strings(conn.Do("SPOP", fmt.Sprintf("%s%s", r.prefixKey, key), n))
 }
 
 // PushNElementToSet will be used to push n elements to set
