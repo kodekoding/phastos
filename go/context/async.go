6c8db9365757cc1be37a13fd14a641462a99b479
Raditya Pratama <radityaapratamaa@gmail.com>
2024-08-31 01:51:10 +0700

    refactor(async-context): fix code to avoid nil pointer reference error

--- go/context/async.go
+++ go/context/async.go
@@ -3,18 +3,26 @@ package context
 import (
 	"context"
 	"github.com/kodekoding/phastos/v2/go/cache"
+	"github.com/kodekoding/phastos/v2/go/common"
 	"github.com/kodekoding/phastos/v2/go/entity"
 	"github.com/kodekoding/phastos/v2/go/notifications"
 )
 
 func CreateAsyncContext(ctx context.Context) context.Context {
 	asyncContext := context.Background()
-	notifCtx := ctx.Value(entity.NotifPlatformContext{}).(notifications.Platforms)
-	cacheCtx := ctx.Value(entity.CacheContext{}).(cache.Caches)
-	jwtCtx := ctx.Value(JwtContext{}).(*entity.JWTClaimData)
+	if notifCtx, exists := ctx.Value(entity.NotifPlatformContext{}).(notifications.Platforms); exists {
+		asyncContext = context.WithValue(asyncContext, entity.NotifPlatformContext{}, notifCtx)
+	}
+	if cacheCtx, exists := ctx.Value(entity.CacheContext{}).(cache.Caches); exists {
+		asyncContext = context.WithValue(asyncContext, entity.CacheContext{}, cacheCtx)
+	}
+	if jwtCtx, exists := ctx.Value(JwtContext{}).(*entity.JWTClaimData); exists {
+		asyncContext = context.WithValue(asyncContext, JwtContext{}, jwtCtx)
+	}
 
-	asyncContext = context.WithValue(asyncContext, entity.NotifPlatformContext{}, notifCtx)
-	asyncContext = context.WithValue(asyncContext, entity.CacheContext{}, cacheCtx)
-	asyncContext = context.WithValue(asyncContext, JwtContext{}, jwtCtx)
+	if traceId, exists := ctx.Value(common.TraceIdKeyContextStr).(string); exists {
+		asyncContext = context.WithValue(asyncContext, common.TraceIdKeyContextStr, traceId)
+	}
+
 	return asyncContext
 }
