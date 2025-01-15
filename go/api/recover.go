74702e80525755d5b015354c0d46910f01231d8d
Raditya Pratama <radityaapratamaa@gmail.com>
2025-01-15 20:59:40 +0700

    refactor(panic-recover): fix the notif detail with sgw attachment for clear error message

--- go/api/recover.go
+++ go/api/recover.go
@@ -2,11 +2,10 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"runtime/debug"
 
+	sgw "github.com/ashwanthkumar/slack-go-webhook"
 	"github.com/rs/zerolog/log"
 
 	context2 "github.com/kodekoding/phastos/v2/go/context"
@@ -15,39 +14,56 @@ import (
 func panicRecover(r *http.Request, traceId string) {
 	if err := recover(); err != nil {
 		stackTrace := string(debug.Stack())
-		b, _ := io.ReadAll(r.Body)
 
-		fields := map[string]interface{}{
-			"Request.Body":  string(b),
-			"Host":          r.Host,
-			"RequestMethod": r.Method,
-			"RequestURI":    r.RequestURI,
-			"Error":         err,
-			"Path":          r.URL.Path,
-		}
+		marshalErr, _ := json.Marshal(err)
+		notifDetail := new(sgw.Attachment)
+		notifDetail.AddField(sgw.Field{
+			Title: "IP",
+			Value: r.RemoteAddr,
+			Short: true,
+		}).AddField(sgw.Field{
+			Title: "Request Method",
+			Value: r.Method,
+			Short: true,
+		}).AddField(sgw.Field{
+			Title: "Request URI",
+			Value: r.RequestURI,
+			Short: true,
+		}).AddField(sgw.Field{
+			Title: "Host",
+			Value: r.Host,
+			Short: true,
+		}).AddField(sgw.Field{
+			Title: "Path",
+			Value: r.URL.Path,
+			Short: true,
+		}).AddField(sgw.Field{
+			Title: "StackTrace",
+			Value: stackTrace,
+		}).AddField(sgw.Field{
+			Title: "Error",
+			Value: string(marshalErr),
+		}).AddField(sgw.Field{
+			Title: "App Version",
+			Value: appVersion,
+			Short: true,
+		})
 
-		panicData := fields
-		panicData["StackTrace"] = stackTrace
-		panicData["IP"] = r.RemoteAddr
-
-		panicData["StackError"] = err
-		ctx := r.Context()
-		byteData, _ := json.Marshal(panicData)
-		notifMsg := fmt.Sprintf(`your API is panic:
-			%s
-			traceID: %s`, string(byteData), traceId)
-		notif := context2.GetNotif(r.Context())
-		allNotifPlatform := notif.GetAllPlatform()
+		asyncCtx := context2.CreateAsyncContext(r.Context())
 		go func() {
+			notif := context2.GetNotif(asyncCtx)
+			allNotifPlatform := notif.GetAllPlatform()
 			for _, service := range allNotifPlatform {
 				if service.IsActive() {
-					if err := service.Send(ctx, notifMsg, nil); err != nil {
+					service.SetTraceId(traceId)
+
+					if err := service.Send(asyncCtx, "your API is panic !!", notifDetail); err != nil {
 						log.Error().Msgf("error when send %s notifications: %s", service.Type(), err.Error())
 					}
 				}
 
 			}
 		}()
-		log.Error().Any("data", fields).Msg("got panic at handler")
+		log.Error().Any("data", notifDetail).Msg("got panic at handler")
 	}
 }
