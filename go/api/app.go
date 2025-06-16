f6605f631b7709ea33a1e9407d4f2d0a17f98c32
Raditya Pratama <radityaapratamaa@gmail.com>
2025-06-16 16:49:06 +0700

    fix(main-app): fix import deprecated module

--- go/api/app.go
+++ go/api/app.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/gorilla/schema"
