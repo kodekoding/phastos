9ce17ef599dd2069ad3c9421dd4dfd06769e2c80
Raditya Pratama <radityaapratamaa@gmail.com>
2025-10-19 17:54:01 +0700

    feat(helper-template): create new helper function for parsing template with funcMaps

--- go/helper/template.go
+++ go/helper/template.go
@@ -3,8 +3,15 @@ package helper
 import (
 	"bytes"
 	"embed"
-	"github.com/pkg/errors"
 	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
 func ParseTemplate(embedFS embed.FS, file string, args interface{}, additionalBodyContent ...string) (bytes.Buffer, error) {
@@ -55,4 +62,68 @@ func ParseFileTemplate(filePath string, args interface{}, additionalBodyContent 
 	}
 
 	return tpl, nil
+}
+
+func ParseTemplateFromPath(filePath string, data any, optionalParams ...any) (strings.Builder, error) {
+	var err error
+	var result strings.Builder
+	name := filepath.Base(filePath)
+	tmpl := template.New(name)
+
+	// checking optional params
+	if optionalParams != nil && len(optionalParams) > 0 {
+		for _, param := range optionalParams {
+			switch value := param.(type) {
+			case string:
+				result.WriteString(value)
+			case template.FuncMap:
+				tmpl.Funcs(value)
+			case map[string]any:
+				var newValue template.FuncMap
+				newValue = value
+				tmpl.Funcs(newValue)
+			default:
+				log.Warn().Any("val", value).Msg("Undefined optional params data type")
+			}
+		}
+	}
+
+	isURLPath := strings.Contains(filePath, "http")
+
+	var templateContent strings.Builder
+	// getting template contents
+	if isURLPath {
+		// if templatePath is url, ex: https://................/file.html
+		resp, err := http.Get(filePath)
+		if err != nil {
+			err = errors.Wrap(err, "phastos.helper.template.ParseTemplateFromPath.GetTemplateFromURL")
+			return result, err
+		}
+		defer resp.Body.Close()
+
+		htmlContent, err := io.ReadAll(resp.Body)
+		if err != nil {
+			err = errors.Wrap(err, "phastos.helper.template.ParseTemplateFromPath.ReadBodyResponseHTML")
+			return result, err
+		}
+		templateContent.Write(htmlContent)
+	} else {
+		// if templatePath is local path, ex: /tmp/templates/file.html
+		contentByte, err := os.ReadFile(filePath)
+		if err != nil {
+			err = errors.Wrap(err, "phastos.helper.template.ParseTemplateFromPath.ReadFileFromLocalPath")
+			return result, err
+		}
+		templateContent.Write(contentByte)
+
+	}
+	parsedTemplate, err := tmpl.Parse(templateContent.String())
+	if err != nil {
+		return result, errors.Wrap(err, "phastos.helper.template.ParseTemplateFromPath.ParseContent")
+	}
+
+	if err = parsedTemplate.Execute(&result, data); err != nil {
+		return result, errors.Wrap(err, "phastos.helper.template.ParseTemplateFromPath.Execute")
+	}
+	return result, nil
 }
