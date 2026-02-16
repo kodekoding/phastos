5eaa30c835e99f8d9ff1f9b0da8da940ba8a17bd
Raditya Pratama <radityaapratamaa@gmail.com>
2026-02-17 00:40:58 +0700

    feat(cron): create + adjust register scheduler, adding new function to remove the scheduler by pattern, and create new interface for the contract to front app implementor

--- go/cron/cron.go
+++ go/cron/cron.go
@@ -3,17 +3,25 @@ package cron
 import (
 	"context"
 	"fmt"
-	"github.com/kodekoding/phastos/v2/go/env"
-	helper2 "github.com/kodekoding/phastos/v2/go/helper"
-	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/kodekoding/phastos/v2/go/env"
+	helper2 "github.com/kodekoding/phastos/v2/go/helper"
+	"github.com/rs/zerolog/log"
 
 	"github.com/robfig/cron/v3"
 )
 
 type (
+	Engines interface {
+		RegisterScheduler(pattern string, handler HandlerFunc)
+		RemoveScheduler(pattern string)
+		Start()
+		Stop()
+		Wrap(wrapper Wrapper)
+	}
 	Options func(*option)
 	option  struct {
 		timezone string
@@ -26,6 +34,7 @@ type (
 		handlerTotal int
 		wrapper      []Wrapper
 		ctx          context.Context
+		handlerList  map[string]cron.EntryID
 	}
 
 	Wrapper interface {
@@ -43,7 +52,7 @@ func New(configOptions ...Options) *Engine {
 	if options.timezone != "" {
 		location, err := time.LoadLocation(options.timezone)
 		if err != nil {
-			log.Fatalln("Failed to load location to run scheduler: ", err.Error())
+			log.Fatal().Err(err).Msg("Failed to load location to run scheduler: ")
 		}
 
 		cronOpts = cron.WithLocation(location)
@@ -63,71 +72,114 @@ func WithTimeZone(timeZone string) Options {
 
 func (eg *Engine) RegisterScheduler(pattern string, handler HandlerFunc) {
 	if eg.engine == nil {
-		log.Fatalln("cron engine is nil")
+		log.Fatal().Msg("engine is nil")
 	}
-	if _, err := eg.engine.AddFunc(pattern, func() {
-		timeoutProcessEnv := os.Getenv("CRON_JOB_TIMEOUT_PROCESS")
-		if timeoutProcessEnv == "" {
-			timeoutProcessEnv = "5"
-		}
 
-		timeoutProcess, _ := strconv.Atoi(timeoutProcessEnv)
-		ctx, cancel := context.WithTimeout(eg.ctx, time.Duration(timeoutProcess)*time.Minute)
-		defer cancel()
-
-		respChan := make(chan *Response)
-		start := time.Now()
-		go func() {
-			_ = helper2.SendSlackNotification(ctx,
-				helper2.NotifMsgType(helper2.NotifInfoType),
-				helper2.NotifTitle("Cron Job Started"),
-				helper2.NotifData(map[string]string{
-					"date": start.Format("2006-01-02 15:04:05"),
-				}),
-			)
-			respChan <- handler(ctx)
-		}()
-
-		select {
-		case <-ctx.Done():
-			_ = helper2.SendSlackNotification(ctx,
-				helper2.NotifMsgType(helper2.NotifWarnType),
-				helper2.NotifTitle("Cron Job Failed (Timeout)"),
-				helper2.NotifData(map[string]string{
-					"date": start.Format("2006-01-02 15:04:05"),
-				}),
-			)
-		case resp := <-respChan:
-			var notifType helper2.SentNotifParamOptions
-			notifType = helper2.NotifMsgType(helper2.NotifInfoType)
-			var msg = "Success"
-			notifChannel := os.Getenv("NOTIFICATION_SLACK_INFO_WEBHOOK")
-			end := time.Since(start)
-			notifData := map[string]string{
-				"Processing Time": fmt.Sprintf("%.2f second(s)", end.Seconds()),
-				"Environment":     env.ServiceEnv(),
-				"Process Name":    resp.processName,
-			}
-			if resp.err != nil {
-				notifType = helper2.NotifMsgType(helper2.NotifErrorType)
-				msg = "Error"
-				notifChannel = os.Getenv("NOTIFICATIONS_SLACK_WEBHOOK_URL")
-				notifData["error"] = resp.err.Error()
-			}
-
-			_ = helper2.SendSlackNotification(ctx,
-				helper2.NotifChannel(notifChannel),
-				helper2.NotifTitle(fmt.Sprintf("Cron Job %s", msg)),
-				helper2.NotifData(notifData),
-				notifType,
-			)
-
-			log.Printf("%s Insert Default Schedule in %.2f second(s)", msg, end.Seconds())
-		}
-	}); err != nil {
-		log.Fatalln("Failed to Register Scheduler Handler: ", err.Error())
+	entryID, err := eg.engine.AddFunc(pattern, func() {
+		eg.wrapperCronHandler(pattern, handler)
+	})
+	if err != nil {
+		log.Fatal().Msgf("Failed to Register Scheduler Handler: %s", err.Error())
 	}
+
+	eg.handlerList[pattern] = entryID
 	eg.handlerTotal++
+}
+
+func (eg *Engine) RemoveScheduler(pattern string) {
+	if eg.engine == nil {
+		log.Fatal().Msg("cron engine is nil")
+	}
+	entryID, ok := eg.handlerList[pattern]
+	if !ok {
+		log.Error().
+			Str("process_name", "[REMOVE-SCHEDULER] ").
+			Str("pattern", pattern).
+			Msg("Pattern isn't registered")
+		return
+	}
+
+	// removing the scheduler id
+	eg.engine.Remove(entryID)
+	delete(eg.handlerList, pattern)
+	eg.handlerTotal--
+	log.Info().
+		Str("process_name", "[REMOVE-SCHEDULER]").
+		Int("handler_total", eg.handlerTotal).
+		Str("pattern", pattern).
+		Msg("scheduler successfully removed")
+
+}
+
+func (eg *Engine) wrapperCronHandler(pattern string, handler HandlerFunc) {
+	timeoutProcessEnv := os.Getenv("CRON_JOB_TIMEOUT_PROCESS")
+	if timeoutProcessEnv == "" {
+		timeoutProcessEnv = "1"
+	}
+
+	timeoutProcess, _ := strconv.Atoi(timeoutProcessEnv)
+	ctx, cancel := context.WithTimeout(eg.ctx, time.Duration(timeoutProcess)*time.Minute)
+	defer cancel()
+
+	respChan := make(chan *Response)
+	start := time.Now()
+	go func() {
+		_ = helper2.SendSlackNotification(ctx,
+			helper2.NotifMsgType(helper2.NotifInfoType),
+			helper2.NotifTitle(fmt.Sprintf("Cron Job %s Started", pattern)),
+			helper2.NotifData(map[string]string{
+				"date": start.Format("2006-01-02 15:04:05"),
+			}),
+		)
+		log.Info().
+			Str("process_name", "[SCHEDULER-RUN]").
+			Str("pattern", pattern).Msg("Cron Job Started")
+
+		respChan <- handler(ctx)
+	}()
+
+	select {
+	case <-ctx.Done():
+		end := time.Since(start)
+		_ = helper2.SendSlackNotification(ctx,
+			helper2.NotifMsgType(helper2.NotifWarnType),
+			helper2.NotifTitle(fmt.Sprintf("Cron Job %s Failed (Timeout)", pattern)),
+			helper2.NotifData(map[string]string{
+				"Processing Time": fmt.Sprintf("%.2f second(s)", end.Seconds()),
+			}),
+		)
+	case resp := <-respChan:
+		var notifType helper2.SentNotifParamOptions
+		notifType = helper2.NotifMsgType(helper2.NotifInfoType)
+		var msg = "Success"
+		notifChannel := os.Getenv("NOTIFICATION_SLACK_INFO_WEBHOOK")
+		end := time.Since(start)
+		notifData := map[string]string{
+			"Processing Time": fmt.Sprintf("%.2f second(s)", end.Seconds()),
+			"Environment":     env.ServiceEnv(),
+			"Process Name":    resp.processName,
+		}
+		if resp.err != nil {
+			notifType = helper2.NotifMsgType(helper2.NotifErrorType)
+			msg = "Error"
+			notifChannel = os.Getenv("NOTIFICATIONS_SLACK_WEBHOOK_URL")
+			notifData["error"] = resp.err.Error()
+		}
+
+		_ = helper2.SendSlackNotification(ctx,
+			helper2.NotifChannel(notifChannel),
+			helper2.NotifTitle(fmt.Sprintf("Cron Job %xs %s", pattern, msg)),
+			helper2.NotifData(notifData),
+			notifType,
+		)
+
+		log.Info().
+			Str("process_name", "[SCHEDULER-RUN]").
+			Str("pattern", pattern).
+			Str("status", msg).
+			Float64("execution_time", end.Seconds()).
+			Msg("Cron Job Finished")
+	}
 }
 
 func (eg *Engine) Wrap(wrapper Wrapper) {
@@ -138,11 +190,11 @@ func (eg *Engine) Start() {
 	for _, wrapper := range eg.wrapper {
 		eg.ctx = wrapper.WrapToContext(eg.ctx)
 	}
-	log.Printf("Cron Job / Scheduler is running, handle %d handler(s)", eg.handlerTotal)
+	log.Info().Int("handler", eg.handlerTotal).Msg("Cron Job / Scheduler is running")
 	eg.engine.Start()
 }
 
 func (eg *Engine) Stop() {
-	log.Println("Cron Job / Scheduler stopped")
+	log.Info().Msg("Cron Job / Scheduler stopped")
 	eg.engine.Stop()
 }
